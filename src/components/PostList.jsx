526f4bc75779f16d744938de8f79ca21af84b7c4
yeabsira-mesfin <yeabsira.mesfin29@gmail.com>
2025-03-02 14:35:32 -0500

    final

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,39 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import Post from "./Post";
 import classes from "../components/PostList.module.css";
 import NewPost from "./NewPost";
-import { useState } from "react";
 import Modal from "./Modal";
+
 const PostList = (props) => {
-  const [text, setText] = useState("");
-  const [author, setAuthor] = useState("");
+  const [posts, setPosts] = useState([]); 
 
-
-
-  function bodychangeHandler(event) {
-    setText(event.target.value);
-  }
-  function authorChangeHandler(event) {
-    setAuthor(event.target.value);
+  function addPostHandler(postData) {
+    setPosts((prevPosts) => [...prevPosts, postData]); 
   }
 
   return (
     <>
       {props.isPosting && (
         <Modal onClose={props.onStopPosting}>
-          <NewPost
-            onBodyChange={bodychangeHandler}
-            onAuthorChange={authorChangeHandler}
-            onCancel={props.onStopPosting}
-          />
+          <NewPost onCancel={props.onStopPosting} onAddPost={addPostHandler} />
         </Modal>
       )}
       <ul className={classes.posts}>
-        <Post name={text} course={author} />
-        <Post
-          name={"Yeabsira working hard to land a remote job"}
-          course={"Yeaba"}
-        />
+        {posts.map((post, index) => (
+          <Post key={index} name={post.body} course={post.author} />
+        ))}
       </ul>
     </>
   );
