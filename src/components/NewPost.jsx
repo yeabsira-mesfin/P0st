2b2fedc623bf89964c04bc67e35922bf4c17f4cd
yeabsira-mesfin <yeabsira.mesfin29@gmail.com>
2025-03-02 09:26:01 -0500

    Body text

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,8 +1,11 @@
+import { useState } from 'react';
 import classes from './NewPost.module.css';
 
 function NewPost() {
+  const [text,setText] = useState('');
+ 
   function changeBodyhandler(event) {
-    console.log(event.target.value);
+   setText(event.target.value);
   }
   return (
     <form className={classes.form}>
@@ -10,6 +13,7 @@ function NewPost() {
         <label htmlFor="body">Text</label>
         <textarea id="body" required rows={3} onChange={changeBodyhandler} />
       </p>
+      <p>{text}</p>
       <p>
         <label htmlFor="name">Your name</label>
         <input type="text" id="name" required />
